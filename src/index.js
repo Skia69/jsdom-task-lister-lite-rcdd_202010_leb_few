0ff68400baa60d84403be11e2c0b5063e7f8d370
Mhamad Ibrahim <skiastep@gmail.com>
2020-11-27 12:07:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const todoInput = document.querySelector("input[type='text']")
+  const submitButton = document.querySelector("input[type='submit']")
+  const todoList = document.querySelector("#tasks")
+  
+  let todos = []
+  
+  const addTodo = e => { 
+    e.preventDefault()
+    todos.push(todoInput.value)
+  } 
+  
+  submitButton.addEventListener('click', addTodo)
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 });
